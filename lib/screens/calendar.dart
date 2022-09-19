b93f85b9a776180d281491fcdad1141c368ad044
Blake Wood Jr <blakewoodjr84@gmail.com>
2022-09-19 14:58:12 -0700

    updates

--- lib/screens/calendar.dart
+++ lib/screens/calendar.dart
@@ -1,5 +1,6 @@
 import 'package:calendar_view/provider/schedule.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
 class CalendarScreen extends StatefulWidget {
@@ -10,17 +11,27 @@ class CalendarScreen extends StatefulWidget {
 }
 
 class _CalendarScreenState extends State<CalendarScreen> {
+  final _controller = ScrollController();
   @override
   void initState() {
     super.initState();
     context.read<ScheduleProvider>().init();
   }
 
+  final startTime = DateTime(2022, 1, 1, 6);
+  final endTime = DateTime(2022, 1, 1, 19);
+
+  static final timesFormatter = DateFormat('h a');
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Calendar Goes Here'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Calendar'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [],
       ),
     );
   }
