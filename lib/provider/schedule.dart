5ac45bc7e1654d340ee9a066e9dafd618b4ba545
Blake Wood Jr <blakewoodjr84@gmail.com>
2022-09-20 15:12:16 -0700

    added schedule items to UI

--- lib/provider/schedule.dart
+++ lib/provider/schedule.dart
@@ -1,16 +1,51 @@
-import 'dart:io';
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 import 'dart:developer' as devtools;
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 
+@immutable
+class ScheduleItem {
+  final DateTime startTime;
+  final DateTime endTime;
+  final String name;
+  final String description;
+
+  const ScheduleItem({
+    required this.startTime,
+    required this.endTime,
+    required this.name,
+    required this.description,
+  });
+
+  ScheduleItem.fromMap(Map<String, dynamic> map)
+      : startTime = DateTime.fromMillisecondsSinceEpoch(map['start_time']),
+        endTime = DateTime.fromMillisecondsSinceEpoch(map['end_time']),
+        name = map['name'],
+        description = map['description'];
+
+  @override
+  String toString() {
+    return 'ScheduleItem(startTime: $startTime, endTime: $endTime, name: $name, description: $description)';
+  }
+}
+
 class ScheduleProvider extends ChangeNotifier {
+  final scheduleList = <ScheduleItem>[];
+
   void init() async {
     final response = await HttpClient()
         .get('localhost', 5500, '/json/schedule.json')
         .then((request) => request.close())
-        .then((response) => response.transform(utf8.decoder).join());
+        .then((response) => response.transform(utf8.decoder).join())
+        .then((string) => jsonDecode(string) as List<dynamic>)
+        .then((list) => list.map((item) => item));
 
-    devtools.log(response.toString());
+    for (final schedule in response) {
+      final scheduleItem = ScheduleItem.fromMap(schedule);
+      scheduleList.add(scheduleItem);
+    }
+    notifyListeners();
   }
 }
--- lib/screens/calendar.dart
+++ lib/screens/calendar.dart
@@ -1,7 +1,10 @@
 import 'package:calendar_view/provider/schedule.dart';
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:linked_scroll_controller/linked_scroll_controller.dart';
 import 'package:provider/provider.dart';
+import 'dart:developer' as devtools;
 
 class CalendarScreen extends StatefulWidget {
   const CalendarScreen({Key? key}) : super(key: key);
@@ -11,10 +14,16 @@ class CalendarScreen extends StatefulWidget {
 }
 
 class _CalendarScreenState extends State<CalendarScreen> {
-  final _controller = ScrollController();
+  late LinkedScrollControllerGroup _controllers;
+  late final ScrollController _scroll1;
+  late final ScrollController _scroll2;
+
   @override
   void initState() {
     super.initState();
+    _controllers = LinkedScrollControllerGroup();
+    _scroll1 = _controllers.addAndGet();
+    _scroll2 = _controllers.addAndGet();
     context.read<ScheduleProvider>().init();
   }
 
@@ -25,13 +34,107 @@ class _CalendarScreenState extends State<CalendarScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    final scheduleList = context.watch<ScheduleProvider>().scheduleList;
+
+    if (scheduleList.isEmpty) {
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Calendar'),
         centerTitle: true,
       ),
-      body: Column(
-        children: [],
+      body: Stack(
+        children: [
+          Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  controller: _scroll1,
+                  itemCount: 12,
+                  itemBuilder: (context, index) {
+                    final time = startTime.add(Duration(hours: index));
+                    return Container(
+                      height: 75,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          width: 1,
+                          color: Colors.purple,
+                        ),
+                      ),
+                      child: Row(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                width: 1,
+                                color: Colors.red,
+                              ),
+                            ),
+                            width: 75,
+                            child: Text(
+                              timesFormatter.format(time),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  controller: _scroll2,
+                  itemCount: 12,
+                  itemBuilder: (context, index) {
+                    final time = startTime.add(Duration(hours: index));
+
+                    // Expand the schedule item to the end time
+
+                    // Find a schedule Item that starts at this time
+                    final item = context
+                        .read<ScheduleProvider>()
+                        .scheduleList
+                        .singleWhereOrNull((schedule) => schedule.startTime.hour == time.hour);
+
+                    // Expand to it's end time. Check to see if it expands past it's end time
+                    final scheduleItemHeight = item == null ? 0 : item.endTime.hour - item.startTime.hour;
+                    devtools.log('ITEM HEIGHT: $scheduleItemHeight');
+
+                    // If an item ends here, reduce by 15
+
+                    return Row(
+                      children: [
+                        // Space for Time
+                        const SizedBox(width: 75),
+
+                        // Space for Schedule Item
+                        Container(
+                          height: scheduleItemHeight > 0 ? 75.0 * scheduleItemHeight : 75.0,
+                          width: size.width * .6,
+                          color: item != null ? Colors.orange : Colors.transparent,
+                        )
+                      ],
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
