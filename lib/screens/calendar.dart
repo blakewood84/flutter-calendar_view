e7709299651105258d1bb74eb878cd4e39356ccf
Blake Wood Jr <blakewoodjr84@gmail.com>
2022-09-21 15:05:33 -0700

    code cleanup

--- lib/screens/calendar.dart
+++ lib/screens/calendar.dart
@@ -19,8 +19,8 @@ class _CalendarScreenState extends State<CalendarScreen> {
   late LinkedScrollControllerGroup _controllers2;
   late final ScrollController _scroll1; // Y
   late final ScrollController _scroll2; // Y
-  late final ScrollController _scroll3; // Y
-  late final ScrollController _scroll4;
+  late final ScrollController _scroll3; // X
+  late final ScrollController _scroll4; // X ->
 
   @override
   void initState() {
@@ -82,13 +82,7 @@ class _CalendarScreenState extends State<CalendarScreen> {
                       child: Row(
                         children: [
                           // Space for Time
-                          Container(
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                width: 1,
-                                color: Colors.red,
-                              ),
-                            ),
+                          SizedBox(
                             width: 75,
                             height: 75,
                             child: Text(timesFormatter.format(time)),
@@ -133,31 +127,17 @@ class _CalendarScreenState extends State<CalendarScreen> {
                     // Expand to it's end time. Check to see if it expands past it's end time
                     final scheduleItemHeight = item == null ? 0 : item.endTime.hour - item.startTime.hour;
 
-                    return Container(
-                      decoration: const BoxDecoration(
-                        border: Border(
-                          top: BorderSide(
-                            width: 1,
-                            color: Colors.black,
+                    return Row(
+                      children: [
+                        // Space for Time
+                        SizedBox(
+                          width: 75,
+                          height: 76,
+                          child: Text(
+                            timesFormatter.format(time),
                           ),
                         ),
-                      ),
-                      child: Row(
-                        children: [
-                          // Space for Time
-                          Container(
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                width: 1,
-                                color: Colors.red,
-                              ),
-                            ),
-                            width: 75,
-                            height: 75,
-                            child: Text(timesFormatter.format(time)),
-                          ),
-                        ],
-                      ),
+                      ],
                     );
                   },
                 ),
@@ -167,118 +147,5 @@ class _CalendarScreenState extends State<CalendarScreen> {
         ],
       ),
     );
-
-    // return Scaffold(
-    //   appBar: AppBar(
-    //     title: const Text('Calendar'),
-    //     centerTitle: true,
-    //   ),
-    //   body: SizedBox(
-    //     width: 600,
-    //     height: size.height,
-    //     child: SingleChildScrollView(
-    //       child: Stack(
-    //         children: [
-    //           Column(
-    //             mainAxisSize: MainAxisSize.max,
-    //             children: [
-    //               Expanded(
-    //                 child: ListView.builder(
-    //                   controller: _scroll1,
-    //                   scrollDirection: Axis.vertical,
-    //                   itemCount: 12,
-    //                   itemBuilder: (context, index) {
-    //                     final time = startTime.add(Duration(hours: index));
-    //                     return Container(
-    //                       height: 75,
-    //                       decoration: BoxDecoration(
-    //                         border: Border.all(
-    //                           width: 1,
-    //                           color: Colors.purple,
-    //                         ),
-    //                       ),
-    //                       child: Row(
-    //                         children: [
-    //                           Container(
-    //                             decoration: BoxDecoration(
-    //                               border: Border.all(
-    //                                 width: 1,
-    //                                 color: Colors.red,
-    //                               ),
-    //                             ),
-    //                             width: 75,
-    //                             child: Text(
-    //                               timesFormatter.format(time),
-    //                             ),
-    //                           ),
-    //                         ],
-    //                       ),
-    //                     );
-    //                   },
-    //                 ),
-    //               )
-    //             ],
-    //           ),
-    //           Column(
-    //             mainAxisSize: MainAxisSize.max,
-    //             children: [
-    //               Expanded(
-    //                 child: ListView.builder(
-    //                   controller: _scroll2,
-    //                   scrollDirection: Axis.vertical,
-    //                   itemCount: 12,
-    //                   itemBuilder: (context, index) {
-    //                     final time = startTime.add(Duration(hours: index));
-    //                     devtools.log('----------------------------');
-    //                     devtools.log('\nRow Start Time: ${time.hour}');
-    //                     // Expand the schedule item to the end time
-
-    //                     // Find a schedule Item that starts at this time
-    //                     final item = context
-    //                         .read<ScheduleProvider>()
-    //                         .scheduleList
-    //                         .singleWhereOrNull((schedule) => schedule.startTime.hour == time.hour);
-
-    //                     if (item != null) {
-    //                       devtools.log('Schedule Item Start Time: ${item.startTime.toIso8601String()}');
-    //                       devtools.log('Schedule Item End Time: ${item.endTime.toIso8601String()}');
-    //                       devtools.log('\n----------------------------');
-    //                     }
-
-    //                     // Expand to it's end time. Check to see if it expands past it's end time
-    //                     final scheduleItemHeight = item == null ? 0 : item.endTime.hour - item.startTime.hour;
-
-    //                     // If an item ends here, reduce by 15
-
-    //                     return Row(
-    //                       children: [
-    //                         // Space for Time
-    //                         const SizedBox(width: 75),
-
-    //                         // Space for Schedule Item
-    //                         Container(
-    //                           height: scheduleItemHeight > 0 ? 75.0 * scheduleItemHeight : 75.0,
-    //                           width: size.width * .6,
-    //                           color: item != null ? Colors.orange : Colors.transparent,
-    //                         ),
-    //                         const SizedBox(width: 10),
-    //                         // Space for Schedule Item
-    //                         Container(
-    //                           height: scheduleItemHeight > 0 ? 75.0 * scheduleItemHeight : 75.0,
-    //                           width: size.width * .6,
-    //                           color: item != null ? Colors.orange : Colors.transparent,
-    //                         )
-    //                       ],
-    //                     );
-    //                   },
-    //                 ),
-    //               )
-    //             ],
-    //           ),
-    //         ],
-    //       ),
-    //     ),
-    //   ),
-    // );
   }
 }
